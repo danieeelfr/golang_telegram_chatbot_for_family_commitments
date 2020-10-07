0ecb6297a829b37f9e6340aa7491b5f912d7760f
DanieeelFR <danielfonsecareis@gmail.com>
2020-10-07 18:55:03 -0300

    foundation

--- src/fn_garbage_collect/function.go
+++ src/fn_garbage_collect/function.go
@@ -10,27 +10,35 @@ import (
 
 // GarbageCollect handle the request and sent back the related response
 func GarbageCollect(res http.ResponseWriter, req *http.Request) {
-
+	log.Println("0")
 	body := &telegramWebHookReqBody{}
 	if err := json.NewDecoder(req.Body).Decode(body); err != nil {
+		log.Println("1")
 		log.Println(err)
 		return
 	}
-
+	log.Println("2")
 	log.Println(body)
-
+	log.Println("3")
 	switch weekday := time.Now().Weekday(); weekday {
 
 	case time.Tuesday, time.Thursday, time.Saturday:
+		log.Println("4")
 		sendTelegramMessage(body.Message.Chat.ID, BuildTelegramMessage(weekday.String()))
+		log.Println("5")
 	default:
 		if body.Message.Text == "/lixo" || body.Message.Text == "/lixeira" ||
 			body.Message.Text == "/rotina" || body.Message.Text == "/tarefas" ||
 			body.Message.Text == "/o que fazer" || body.Message.Text == "/lembrar" {
+			log.Println("6")
 			sendTelegramMessage(body.Message.Chat.ID, BuildTelegramMessage(weekday.String()))
+			log.Println("7")
 		}
+		log.Println("8")
 	}
+	log.Println("9")
 
 	log.Println(res, html.EscapeString("Finished..."))
+	log.Println("10")
 	return
 }
--- src/fn_garbage_collect/telegram.go
+++ src/fn_garbage_collect/telegram.go
@@ -23,48 +23,56 @@ type sendMessageReqBody struct {
 }
 
 func sendTelegramMessage(chatID int64, text string) {
-
+	log.Println("13")
 	reqBody := &sendMessageReqBody{
-		ChatID:    chatID,
+		ChatID:    -467000473,
 		Text:      text,
 		ParseMode: "Markdown",
 	}
 
 	reqBytes, err := json.Marshal(reqBody)
 	if err != nil {
+		log.Println("14")
 		log.Println("An error occured (json.Marshal)")
 		log.Println(err)
 		return
 	}
+	log.Println("15")
 
 	log.Println(reqBytes)
-	res, err := http.Post("https://api.telegram.org/bot1300731345:AAHbTJAXAambzUkUoQN6StC4Q0aR6o0tcSU/sendMessage", "application/json", bytes.NewBuffer(reqBytes))
+	res, err := http.Post("https://api.telegram.org/bot1157412439:AAFo2ZQVvM11KcX0U_BGKkTQfdv25ul2_Y0/sendMessage", "application/json", bytes.NewBuffer(reqBytes))
 
 	if err != nil {
+		log.Println("16")
 		log.Println("An error occured (calling api with POST)")
 		log.Println(err)
 		return
 	}
 
+	log.Println("17")
+
 	if res.StatusCode != http.StatusOK {
+		log.Println("18")
 		log.Println(res.Status)
 		log.Println(err)
 		return
 	}
-
+	log.Println("19")
 	defer res.Body.Close()
-
+	log.Println("20")
 	return
 }
 
 // BuildTelegramMessage bla
 func BuildTelegramMessage(weekday string) string {
+	log.Println("11")
 	const EmojiSun string = "\xE2\x98\x80"
 	const EmojiRecicle string = "\xE2\x99\xBB"
 
 	greetings := EmojiSun + " Bom dia! " + EmojiSun + "\n\n"
 	message := "Hoje é *" + weekday + "*! Espero que você tenha um ótimo dia!\n\n"
 	footer := "Por favor, *lembre-se de levar o lixo para fora.* " + EmojiRecicle
-
+	log.Println("12")
 	return greetings + message + footer
+
 }
